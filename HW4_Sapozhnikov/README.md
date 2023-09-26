d98ccaf8f9e3784252127c2639c2c843a9188dce
Nikita <nikita.sapozhnikov1@gmail.com>
2023-09-26 07:14:08 +0300

    Initial commit

---
+++ HW4_Sapozhnikov/README.md
@@ -0,0 +1,19 @@
+# HW 4. Functions 2
+> *This is the repo for the fourth homework of the BI Python 2023 course*
+
+### Title
+
+### Overview 
+
+### Usage
+
+### Options
+
+### Examples
+
+### Troubleshooting
+
+### Contacts
+
+Feel free to report any bugs and problems encountered.
+Email: nikita.sapozhnikov1@gmail.com
---
+++ HW4_Sapozhnikov/prototool.py
@@ -0,0 +1,16 @@
+def main():
+    """
+    an entry point to the tool
+
+    This tool provides the following functionality:
+    - local alignment of two sequences
+    - ...
+
+    To get started choose one of the possible programms to run:
+    1. Local alignment
+    Enter two protein sequences in 1- letter encoding. The code will return alignment scores and 
+    sequences aligned on each other. 
+    2. ...
+
+    """
+    pass
