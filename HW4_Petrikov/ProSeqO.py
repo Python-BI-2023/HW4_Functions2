1786d8a96994ba3fb1314678a090ca4398a263ee
PKbioscience <bioscience.kp@gmail.com>
2023-09-27 00:55:24 +0300

    Add calc_total_charge function

--- HW4_Petrikov/ProSeqO.py
+++ HW4_Petrikov/ProSeqO.py
@@ -3,7 +3,7 @@ AMINOACIDS_NAMES = {'A': 'Ala', 'R': 'Arg', 'N': 'Asn', 'D': 'Asp', 'V': 'Val', 
                     'T': 'Thr', 'W': 'Trp', 'F': 'Phe', 'C': 'Cys'}
 
 GRAVY_AA_VALUES = {'L': 3.8, 'K': -3.9, 'M': 1.9, 'F': 2.8, 'P': -1.6, 'S': -0.8, 'T': -0.7, 'W': -0.9, 'Y': -1.3,
-                   'V': 4.2, 'A': 1.8, 'R': -4.5, 'N': -3.5, 'D': -3.5,  'C': 2.5, 'Q': -3.5,  'E': -3.5, 'G': -0.4,
+                   'V': 4.2, 'A': 1.8, 'R': -4.5, 'N': -3.5, 'D': -3.5, 'C': 2.5, 'Q': -3.5, 'E': -3.5, 'G': -0.4,
                    'H': -3.2, 'I': 4.5}
 
 
@@ -18,12 +18,30 @@ def calc_gravy(amino_ac_seq: str) -> float:
     return round(gravy_aa_sum / len(amino_ac_seq), 3)
 
 
+def calc_total_charge(charged_amino_ac_list: list, pH_value: float) -> float:
+    """
+    Calculates approximate total charge of given protein
+    based on the number of key charged amino acids
+    """
+    N_terminal_charge = 1 / (1 + 10 ** (pH_value - 8.2))
+    C_terminal_charge = -1 / (1 + 10 ** (3.65 - pH_value))
+    Cys_charge = -charged_amino_ac_list[0] / (1 + 10 ** (8.18 - pH_value))
+    Asp_charge = -charged_amino_ac_list[1] / (1 + 10 ** (3.9 - pH_value))
+    Glu_charge = -charged_amino_ac_list[2] / (1 + 10 ** (4.07 - pH_value))
+    Tyr_charge = -charged_amino_ac_list[3] / (1 + 10 ** (10.46 - pH_value))
+    His_charge = charged_amino_ac_list[4] / (1 + 10 ** (pH_value - 6.04))
+    Lys_charge = charged_amino_ac_list[5] / (1 + 10 ** (pH_value - 10.54))
+    Arg_charge = charged_amino_ac_list[6] / (1 + 10 ** (pH_value - 12.48))
+    total_charge = N_terminal_charge + C_terminal_charge + Cys_charge + Asp_charge + Glu_charge + Tyr_charge + His_charge + Lys_charge + Arg_charge
+    return total_charge
+
+
 def transform_to_three_letter(sequence: str) -> str:
-    '''
+    """
     Transform 1-letter aminoacid symbols in
     sequence to 3-letter symbols separated by
     hyphens.
-    '''
+    """
     new_protein = ''
     for aminoacid in sequence:
         new_protein += AMINOACIDS_NAMES[aminoacid] + '-'
@@ -31,18 +49,18 @@ def transform_to_three_letter(sequence: str) -> str:
 
 
 def sequence_length(sequence: str) -> int:
-    '''
+    """
     Function counts number of aminoacids in
     inputed sequence
-    '''
+    """
     return len(sequence)
 
 
 def longest_sequence(sequences: list) -> str:
-    '''
+    """
     Function returns longest protein sequence,
     if there is only one sequence, function
     returns it.
-    '''
+    """
     sequences.sort(key=len, reverse=True)
     return sequences[0]
