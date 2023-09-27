399b7f2b90233e9e59df428b5d672a2ee8a8182e
Michil Trofimov <trofimov.michil@gmail.com>
2023-09-27 16:43:45 +0300

    Add needleman-wunsch function

--- HW4_Trofimov/das_protein_tools.py
+++ HW4_Trofimov/das_protein_tools.py
@@ -58,7 +58,7 @@ def build_scoring_matrix(
     amino_acid_alphabet: str = "ACDEFGHIKLMNPQRSTVWY",
 ) -> dict:
     """
-    Build a default scoring matrix, if not provided in needleman-wunsch function parameter, as a dictionary of dictionaries
+    Build a default scoring matrix, if not provided in needleman-wunsch function parameter
 
     Args:
     - match_score (int): integer value of a matching score of aminoacids
@@ -66,7 +66,7 @@ def build_scoring_matrix(
     - amino_acid_alphabet (str): upper case amino acid alphabet
 
     Returns:
-    - dictionary of dictionaries of aminoacids scores. In which this dictionary contain aminoacid as a key and its value a dictionary of scores
+    - a dictionary of dictionaries representing a scoring matrix for aminoacids paris. Key of a dictionary is an aminoacid and its value is a dictionary of scores
     """
 
     scoring_matrix = {}
@@ -79,3 +79,101 @@ def build_scoring_matrix(
             )
 
     return scoring_matrix
+
+
+def needleman_wunsch(
+    seq1: str,
+    seq2: str,
+    scoring_matrix: dict = None,
+    gap_penalty: int = -1,
+    match_score: int = 1,
+    mismatch_score: int = -1,
+) -> str:
+    """
+    Uses Needleman-Wunsch algorithm to make a global alignment of two sequences.
+
+    Args:
+    - seq1 (str): first aminoacid sequence for alignment
+    - seq2 (str): second aminoacid sequence for alignment
+    - scoring_matrix (dict): A dictionary representing a scoring matrix for amino acid pairs
+      If not provided, a default scoring_matrix is generated based on match and mismatch scores
+    - gap_penalty (int): integer va;ue of a penalty score for introducing a gap in the alignment
+    - match_score (int): integer value of a matching score for matching aminoacids
+    - mismatch_score (int): integer value of a mismatching score for mismatched aminoacids
+
+    Returns:
+    - string: a string containing the aligned sequences (str), the aligned score (int)
+    """
+    if scoring_matrix is None:
+        # Default scoring matrix if not provided
+        scoring_matrix = build_scoring_matrix(match_score, mismatch_score)
+
+    seq1_upper = seq1.upper()  # Convert seq1 to uppercase
+    seq2_upper = seq2.upper()  # Convert seq2 to uppercase
+
+    m, n = len(seq1_upper), len(seq2_upper)
+
+    # Initialize matrices
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    traceback = [[""] * (n + 1) for _ in range(m + 1)]
+
+    # Fill in the scoring matrix and traceback matrix
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            match = dp[i - 1][j - 1] + scoring_matrix.get(seq1_upper[i - 1], {}).get(
+                seq2_upper[j - 1], mismatch_score
+            )
+            delete = dp[i - 1][j] + gap_penalty
+            insert = dp[i][j - 1] + gap_penalty
+
+            dp[i][j] = max(match, delete, insert)
+
+            if dp[i][j] == match:
+                traceback[i][j] = "D"  # Diagonal (indicates a match/mismatch)
+            elif dp[i][j] == delete:
+                traceback[i][j] = "U"  # Up (indicates a gap in seq2)
+            else:
+                traceback[i][j] = "L"  # Left (indicates a gap in seq1)
+
+    # Traceback to find the aligned sequences while preserving case
+    aligned_seq1, aligned_seq2 = [], []
+    i, j = m, n
+    while i > 0 or j > 0:
+        if i > 0 and j > 0 and traceback[i][j] == "D":
+            # check original case of amionacid in seq1
+            if seq1[i - 1].isupper():
+                aligned_seq1.append(seq1_upper[i - 1])
+            else:
+                aligned_seq1.append(seq1[i - 1])
+
+            # check original case of amionacid in seq2
+            if seq2[j - 1].isupper():
+                aligned_seq2.append(seq2_upper[j - 1])
+            else:
+                aligned_seq2.append(seq2[j - 1])
+
+            i -= 1
+            j -= 1
+        elif i > 0 and traceback[i][j] == "U":
+            # check original case of amionacid in seq1
+            if seq1[i - 1].isupper():
+                aligned_seq1.append(seq1_upper[i - 1])
+            else:
+                aligned_seq1.append(seq1[i - 1])
+            aligned_seq2.append("-")
+
+            i -= 1
+        else:
+            aligned_seq1.append("-")
+            # check original case of amionacid in seq2
+            if seq2[j - 1].isupper():
+                aligned_seq2.append(seq2_upper[j - 1])
+            else:
+                aligned_seq2.append(seq2[j - 1])
+
+            j -= 1
+
+    aligned_seq1 = "".join(reversed(aligned_seq1))
+    aligned_seq2 = "".join(reversed(aligned_seq2))
+
+    return f"{aligned_seq1}, {aligned_seq2}, final score: {dp[m][n]}"
