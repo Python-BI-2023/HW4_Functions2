09d4711b1bc56ce45fdee0023e0d58203ded58ae
Daria <nekrasovadasha22@mail.ru>
2023-09-29 23:21:47 +0300

    add from_proteins_seqs_to_rna and isoelectric_point_determination functions

--- HW4_Sapozhnikov/for_test.py
+++
@@ -1,6 +0,0 @@
-my_input = 'vla', 'ValTrpPhe', 'phe', 'vla', 'ValTrpPhe', 'phe'
-
-for aminoacids in my_input:
-    devided = [aminoacids[i:i + 3] for i in range(0, len(aminoacids), 3)]
-    print(devided)
-
