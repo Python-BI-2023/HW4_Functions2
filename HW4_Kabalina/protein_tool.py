5952d560980d8dde760e8e47da983b71ea4f8a2f
Stacy <stacy.petukhova@gmail.com>
2023-09-29 16:56:59 +0300

    Calculate the relative amino acid composition of protein

--- HW4_Kabalina/protein_tool.py
+++ HW4_Kabalina/protein_tool.py
@@ -20,3 +20,28 @@ def calculate_amino_acid_percentages(seq: str) -> str:
     output = ', '.join([ f'{key}: {round(value, 2)}' for key, value in sorted(composition_rates.items(),
                                                key=lambda item: -item[1])])
     return output
+
+
+def classify_amino_acid(seq: str) -> str:
+    """
+    Determine the percentage of acidic, basic and neutral amino acids in protein.
+
+    Arguments:
+    - seq (str): amino acid sequence. The input must be uppercased and use the single letter amino acid code.
+
+    Returns
+    - output (str): percentage of neutral, acidic and basic amino acids in protein.
+    """
+    amino_acid_counts = {'acidic': 0, 'neutral': 0, 'basic': 0}
+    for amino_acid in seq:
+        if amino_acid in ['D', 'E']:
+            amino_acid_counts['acidic'] += 1
+        elif amino_acid in ['A', 'N', 'C', 'Q', 'G', 'I', 'L', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V']:
+            amino_acid_counts['neutral'] += 1
+        elif amino_acid in ['H','R', 'K']:
+            amino_acid_counts['basic'] += 1
+    acidic_percentage = round(amino_acid_counts['acidic'] / len(seq) * 100, 2)
+    neutral_percentage = round(amino_acid_counts['neutral'] / len(seq) * 100, 2)
+    basic_percentage = round(amino_acid_counts['basic'] / len(seq) * 100, 2)
+    output = f'neutral: {neutral_percentage}, acidic: {acidic_percentage}, basic: {basic_percentage}'
+    return output
