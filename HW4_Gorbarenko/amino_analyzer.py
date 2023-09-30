dbad46c5104e9e8cf4b936e0f34cff80316dace6
iliapopov17 <iljapopov17@gmail.com>
2023-09-30 10:18:07 +0300

    Add main function to run the tool
    Add 'run_amino_analyzer()' function - the main function to run the amino-analyzer.py tool.

--- HW4_Gorbarenko/amino_analyzer.py
+++ HW4_Gorbarenko/amino_analyzer.py
@@ -264,3 +264,41 @@ def sulphur_containing_aa_counter(sequence):
             counter += 1
     answer = str(counter)
     return 'The number of sulphur-containing amino acids in the sequence is equal to ' + answer
+
+def run_amino_analyzer(sequence, procedure, *, weight_type = 'average'):
+    """
+    This is the main function to run the amino-analyzer.py tool.
+    
+    Args:
+        sequence (str): The input protein sequence in one-letter code.
+        procedure (str): amino-analyzer.py tool has 5 functions at all:
+            1. aa_weight - Calculate the amino acids weight in a protein sequence.
+            2. count_hydroaffinity - Count the quantity of hydrophobic and hydrophilic amino acids in a protein sequence.
+            3. peptide_cutter - This function identifies cleavage sites in a given peptide sequence using a specified enzyme.
+            4. one_to_three_letter_code - This function converts a protein sequence from one-letter amino acid code to three-letter code.
+            5. sulphur_containing_aa_counter - This function counts sulphur-containing amino acids in a protein sequence.
+        weight_type = 'average': default argument for 'aa_weight' function. weight_type = 'monoisotopic' can be used as a second option.
+        
+    Returns:
+        The result of the procedure.
+    """
+
+    procedures = ['aa_weight', 'count_hydroaffinity', 'peptide_cutter', 'one_to_three_letter_code', 'sulphur_containing_aa_counter']
+    if procedure not in procedures:
+        raise ValueError(f"Incorrect procedure. Acceptable procedures: {', '.join(procedures)}")
+
+    for i in sequence:
+        if not is_aa(sequence):
+            raise ValueError("Incorrect sequence. Only amino acids are allowed (V, I, L, E, Q, D, N, H, W, F, Y, R, K, S, T, M, A, G, P, C, v, i, l, e, q, d, n, h, w, f, y, r, k, s, t, m, a, g, p, c).")
+
+    if procedure == 'aa_weight':
+        result = aa_weight(sequence, weight_type)
+    elif procedure == 'count_hydroaffinity':
+        result = count_hydroaffinity(sequence)
+    elif procedure == 'peptide_cutter':
+        result = peptide_cutter(sequence)
+    elif procedure == 'one_to_three_letter_code':
+        result = one_to_three_letter_code(sequence)
+    elif procedure == 'sulphur_containing_aa_counter':
+        result = sulphur_containing_aa_counter(sequence)
+    return result
