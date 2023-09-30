3d91794a45c3a9978818511461c8d88323e0962c
Dasha <kalabanova_dasha@mail.ru>
2023-09-30 17:29:27 +0300

    Add docstrings and typing to functions

--- protein_analysis_tool.py
+++ protein_analysis_tool.py
@@ -1,5 +1,14 @@
+def get_amino_acid_sum(protein_sequences: list) -> None:
+    """
+    Counts the amount of each amino acid in the injected protein sequences
 
-def get_amino_acid_sum(protein_sequences):
+    Arguments:
+    - protein_sequences (list): list of injected protein sequence
+
+    Return:
+    - None
+    - Only print the amount of each amino acid in the injected protein sequences
+    """
     for protein_sequence in range(len(protein_sequences)):
         dictionary = {'A': 0, 'C': 0, 'D': 0, 'E': 0, 'F': 0, 'G': 0, 'H': 0, 'I': 0,
                       'K': 0, 'L': 0, 'M': 0, 'N': 0, 'P': 0, 'Q': 0, 'R': 0, 'S': 0,
@@ -30,61 +39,78 @@ def get_amino_acid_sum(protein_sequences):
             }
         print('количество аминокислот в последовательности ', protein_sequence + 1, ':')
         for key, value in clone.items():
-            print(key,value)
+            print(key, value)
 
 
+def beautiful_print(codon_optimization_list: list) -> None:
+    """
+            Makes a user-friendly output of a codon-optimized DNA sequence
 
-def beautiful_print(codon_optimization_list):
+            Arguments:
+            - codon_optimization_list (list): list of codon-optimized protein sequence
+
+            Return:
+            - None
+            - Only print the number of the introduced protein sequence and the codon-optimized DNA sequence
+            """
     for nucleotide_sequence in range(len(codon_optimization_list)):
         print('sequence ', nucleotide_sequence + 1)
         print(codon_optimization_list[nucleotide_sequence])
 
 
+def codon_optimization(protein_sequences, cell_type):
+    """
+    Makes codon-optimized DNA based on the introduced amino acid sequences for 3 types of cells:
+    Esherichia coli, Pichia pastoris, Mouse
 
-def codon_optimization(protein_sequences):
-    cell_type = input('Введите вид организма для оптимизации кодонов: ')
+    Arguments:
+    - protein_sequences (list): list of injected protein sequence
+    - cell_type (str): user-entered cell type for codon optimization
+
+    Return:
+    - None
+    - Only print the number of the introduced protein sequence and the codon-optimized DNA sequence
+    """
 
     if cell_type == 'Esherichia coli' or cell_type == 'E.coli':
         codon_optimization_Ecoli = []
-        Ecoli_triplets = {'A': 'GCG', 'C': 'TGC', 'D': 'GAT', 'E': 'GAA', 'F': 'TTT', 'G': 'GGC',
+        ecoli_triplets = {'A': 'GCG', 'C': 'TGC', 'D': 'GAT', 'E': 'GAA', 'F': 'TTT', 'G': 'GGC',
                           'H': 'CAT', 'I': 'ATT', 'K': 'AAA', 'L': 'CTG', 'M': 'ATG', 'N': 'AAC',
                           'P': 'CCG', 'Q': 'CAG', 'R': 'CGT', 'S': 'AGC', 'T': 'ACC', 'V': 'GTG',
                           'W': 'TGG', 'Y': 'TAT'}
-        replacer_Ecoli = Ecoli_triplets.get
+        replacer_ecoli = ecoli_triplets.get
         for amino_acid in range(len(protein_sequences)):
-            codon_optimization_Ecoli += [''.join([replacer_Ecoli(n, n) for n in protein_sequences[amino_acid]])]
-        beautiful_print(codon_optimization_Ecoli)
+            codon_optimization_Ecoli += [''.join([replacer_ecoli(n, n) for n in protein_sequences[amino_acid]])]
+        return beautiful_print(codon_optimization_Ecoli)
 
     if cell_type == 'Pichia pastoris' or cell_type == 'P.pastoris':
-        codon_optimization_Ppastoris = []
-        Ppastoris_triplets = {'A': 'GCT', 'C': 'TGT', 'D': 'GAT', 'E': 'GAA', 'F': 'TTT', 'G': 'GGT',
+        codon_optimization_ppastoris = []
+        ppastoris_triplets = {'A': 'GCT', 'C': 'TGT', 'D': 'GAT', 'E': 'GAA', 'F': 'TTT', 'G': 'GGT',
                               'H': 'CAT', 'I': 'ATT', 'K': 'AAG', 'L': 'TTG', 'M': 'ATG', 'N': 'AAC',
                               'P': 'CCA', 'Q': 'CAA', 'R': 'AGA', 'S': 'TCT', 'T': 'ACT', 'V': 'GTT',
                               'W': 'TGG', 'Y': 'TAC'}
-        replacer_Ppastoris = Ppastoris_triplets.get
+        replacer_ppastoris = ppastoris_triplets.get
         for amino_acid in range(len(protein_sequences)):
-            codon_optimization_Ppastoris += [''.join([replacer_Ppastoris(n, n) for n in protein_sequences[amino_acid]])]
-        beautiful_print(codon_optimization_Ppastoris)
+            codon_optimization_ppastoris += [''.join([replacer_ppastoris(n, n) for n in protein_sequences[amino_acid]])]
+        return beautiful_print(codon_optimization_ppastoris)
 
     if cell_type == 'Mouse' or cell_type == 'mouse':
-        codon_optimization_Mouse = []
-        Mouse_triplets = {'A': 'GCC', 'C': 'TGC', 'D': 'GAC', 'E': 'GAG', 'F': 'TTC', 'G': 'GGC',
+        codon_optimization_mouse = []
+        mouse_triplets = {'A': 'GCC', 'C': 'TGC', 'D': 'GAC', 'E': 'GAG', 'F': 'TTC', 'G': 'GGC',
                           'H': 'CAC', 'I': 'ATC', 'K': 'AAG', 'L': 'CTG', 'M': 'ATG', 'N': 'AAC',
                           'P': 'CCC', 'Q': 'CAG', 'R': 'CGG', 'S': 'AGC', 'T': 'ACC', 'V': 'GTG',
                           'W': 'TGG', 'Y': 'TAC'}
-        replacer_Mouse = Mouse_triplets.get
+        replacer_Mouse = mouse_triplets.get
         for amino_acid in range(len(protein_sequences)):
-            codon_optimization_Mouse += [''.join([replacer_Mouse(n, n) for n in protein_sequences[amino_acid]])]
-        beautiful_print(codon_optimization_Mouse)
+            codon_optimization_mouse += [''.join([replacer_Mouse(n, n) for n in protein_sequences[amino_acid]])]
+        return beautiful_print(codon_optimization_mouse)
     else:
         print('Для оптимизации кодонов доступны следующие виды организмов: Esherichia coli, Pichia pastoris, Mouse')
 
 
+codon_optimization(['MSRQEADLKVSIKKACSTEEAAPK', 'RKHVRACIVFTWDHRSSKAFYNGLRLL'], 'E.coli')
+# print(b)
 
-
-b = get_amino_acid_sum(['MSRQEADLKVSIKKACSTEEAAPK','RKHVRACIVFTWDHRSSKAFYNGLRLL'])
-#print(b)
-
-#for i in range(len(b)):
- #   print('sequence ', i+1)
-  #  print(b[i])
+# for i in range(len(b)):
+#   print('sequence ', i+1)
+#  print(b[i])
