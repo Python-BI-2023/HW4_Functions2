0aae778f1ec4013447255508c27fd124e245312b
Anastasia Shtompel <143889922+anshtompel@users.noreply.github.com>
2023-10-01 11:31:55 +0300

    add new functions, check for mistakes

--- HW4_Shtompel/protein_tools.py
+++ HW4_Shtompel/protein_tools.py
@@ -15,10 +15,6 @@ AA_ALPHABET = {'Nonpolar': ['G', 'A', 'V', 'I', 'L', 'P'],
 
 ALL_AMINOACIDS = set(('G', 'A', 'V', 'I', 'L', 'P', 'S', 'T', 'C', 'M', 'N', 'Q', 'F', 'W', 'Y', 'D', 'E', 'K', 'R', 'H'))
 
-FEATURE_FUNCTIONS = ['count_seq_length', 'classify_aminoacids', 'check_unusual_aminoacids', 'count_charge',
-                     'count_protein_mass', 'count_aliphatic_index', 'count_trypsin_sites']
-
-
 AMINO_ACIDS_MASSES = {
     'G': 57.05, 'A': 71.08, 'S': 87.08, 'P': 97.12, 'V': 99.13,
     'T': 101.1, 'C': 103.1, 'L': 113.2, 'I': 113.2, 'N': 114.1,
@@ -99,6 +95,72 @@ def count_charge(seq: str) -> int:
     return sum_charge
 
 
+def count_protein_mass(seq: str, kda_scale = False) -> float:
+    """
+    Calculates mass of all aminoacids of input peptide in g/mol or KDa scale.
+    Arguments:
+    - seq (str): one-letter code peptide sequence, case is not important;
+    - kda_scale (bool): if True converts peptide mass into kDa scale (1KDa = 1000g/mol).
+    Output:
+    Returns mass of peptide (float).
+    """
+    aa_mass = 0
+    for aminoacid in seq.upper():
+        if aminoacid in AMINO_ACIDS_MASSES:
+            aa_mass += AMINO_ACIDS_MASSES[aminoacid]
+    if kda_scale is True:
+        kda = round(aa_mass / 1000, 1)
+        return kda
+    return aa_mass
+
+
+def count_aliphatic_index(seq: str) -> float:
+    """
+    Calculates aliphatic index - relative proportion of aliphatic aminoacids in input peptide.
+    The higher aliphatic index the higher thermostability of peptide.
+    Argument:
+    - seq (str): one-letter code peptide sequence, letter case is not important.
+    Output:
+    Returns alipatic index (float).
+    """
+    ala_count = seq.count('A') / len(seq)
+    val_count = seq.count('V') / len(seq)
+    lei_count = seq.count('L') / len(seq)
+    izlei_count = seq.count('I') / len(seq)
+    aliph_index = ala_count + 2.9 * val_count + 3.9 * lei_count + 3.9 * izlei_count
+    return aliph_index
+
+
+def not_trypsin_cleaved(seq: str) -> int:
+    """
+    Counts non-cleavable sites of trypsin: Arginine/Proline (RP) and Lysine/Proline (KP) pairs.
+    Argument:
+    - seq (str): one-letter code peptide sequence, case is not important.
+    Output:
+    Returns number of exception sites that cannot be cleaved by trypsin (int).
+    """
+    not_cleavage_count = 0
+    not_cleavage_count += seq.upper().count('RP')
+    not_cleavage_count += seq.upper().count('KP')
+    return not_cleavage_count
+
+
+def count_trypsin_sites(seq: str) -> int:
+    """
+    Counts number of valid trypsin cleavable sites:
+    Arginine/any aminoacid and Lysine/any aminoacid (except Proline).
+    Argument:
+    - seq (str): one-letter code peptide sequence, case is not important.
+    Output:
+    Returns number of valid trypsin cleavable sites (int).
+    If peptide has not any trypsin cleavable sites, it will return zero.
+    """
+    arginine_value = seq.upper().count('R')
+    lysine_value = seq.upper().count('K')
+    count_cleavage = arginine_value + lysine_value - not_trypsin_cleaved(seq)
+    return count_cleavage
+  
+
 OPERATIONS = {'count_protein_mass':count_protein_mass,
              'count_aliphatic_index': count_aliphatic_index,
              'count_trypsin_sites': count_trypsin_sites,
@@ -121,7 +183,7 @@ def protein_tools(*args):
         if not is_protein(seq):
             raise ValueError("One of these sequences is not protein sequence or does not match the rools of input. Please select another sequence.")
         else:
-            if operation in FEATURE_FUNCTIONS:
+            if operation in OPERATIONS:
                 parsed_seq_list.append(OPERATIONS[operation](seq))
             else:
                 raise ValueError("This procedure is not available. Please choose another procedure.")
